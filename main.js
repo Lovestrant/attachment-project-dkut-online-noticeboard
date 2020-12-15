7f043143b6b7a6f207361681226c25551e8fd1a9
Kemboi Lovestrant <kemboilovestrant@gmail.com>
2020-12-15 08:49:33 -0800

    Final commit

--- main.js
+++ main.js
@@ -1,60 +1,9 @@
-/*function attachfiles(event) {
-
-	var demo = document.getElementById("demo");
-	var button = document.getElementById("button");
-	var inputfile = document.getElementById('files');
-
-	button.addEventListener("click", function(){
-
-	var newFile = document.createElement("p");
-	newFile.innerHTML = inputfile.value;
-	demo.src = URL.createObjectURL(event.target.files[0]);
-	demo.appendChild(newFile);
-	inputfile.value = "";
-
-	var group = document.getElementById('demo');
-})
-}
-
-
-function attachfiles(event) {
-	var button = document.getElementById("button");
-
-	button.addEventListener("click", function(){
-	var image = document.getElementById('files').value;
-	image.src = URL.createObjectURL(event.target.files[0]);
-	var demo = document.getElementById("demo");
-	var newFile = document.createElement("p");
-	newFile.setAttribute("img", "");
-	newFile.getAttribute("img", image).style.height = 'auto';
-	newFile.appendChild(image);
-	newFile.appendChild(demo);
-	image.value = "";
-	var group = document.getElementById('demo');
-	})
-
-}
-
-*/
-
-var loadFile = function(event) {
-/**	var demo = document.getElementById("demo");
-	demo.createElement("p");
-	demo.setAttribute("img", "");
-	demo.getAttribute("img").setAttribute("id", "output"); */
-	
-	var image = document.getElementById("output");
-	image.src = URL.createObjectURL(event.target.files[0]);
-	image.value = "";
-
-};
-
 
 function createtext(){
 
-	var demo = document.getElementById("demo");
+	var postsclass3 = document.getElementById("postsclass3");
 	var textbox = document.getElementById("textbox");
-	var button = document.getElementById("button");
+	var button = document.getElementById("button3");
 	//var display = document.getElementById("display");
 
 	button.addEventListener("click", function()
@@ -63,17 +12,9 @@ function createtext(){
 	var newMessage = document.createElement('div');
 	newMessage.style.margin = '80px';
 	newMessage.innerHTML = textbox.value;
-	demo.appendChild(newMessage);
+	postsclass3.appendChild(newMessage);
 	textbox.value = "";
-	var group = document.getElementById('demo');
+	var group = document.getElementById('postsclass3');
 	
-})
-
- }
-
- function adminrole() {
-
- 	createtext();
- 	attachfiles();
- 	
- }
+});
+}
